3daada37f42042bea767b20c55ff91a9561d2469
martin barbet <barbetmartin@gmail.com>
2026-02-19 13:11:28 +0100

    Added Exercise 7

--- Week-1/Exercises/Exercise-7.py
+++ Week-1/Exercises/Exercise-7.py
@@ -7,3 +7,23 @@ should be programmed to keep guessing at it until it finds the number you are th
 At every step, add comments reflecting the logic of what the particular line of code is (supposed 
 to be) doing. 
 """
+
+from random import randint
+
+def computer():
+    while True :
+        guess = randint(1, 100)
+        print('Is it ' + str(guess) + '?')
+        answer = input('press "y" or "n": ')
+        if answer == 'n':
+            print('Hmmm lemme try again...')
+        else :
+            print('Found it!')
+            break
+    
+
+
+print('Think of a number between 1 and 100, press enter when you are ready !')
+input()
+print('I will now try to guess what number it is')
+computer()
